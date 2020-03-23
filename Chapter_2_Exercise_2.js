b602a5b898e106b0df1787da7fac7643c2eb8f4f
Joseph Stefani <jstefan@corp.costco.com>
2020-03-23 15:36:33 -0700

    Updating exercise #2

--- Chapter_2_Exercise_2.js
+++ Chapter_2_Exercise_2.js
@@ -1,5 +1,5 @@
 /* Store a set of words in an array and display the contents both forward and backward. */
 
 var words = ["This", "is", "an", "array", "of", "words"];
-var forward = words.join();
-var reverse = words.reverse();
+var forward = words.join(" ");
+var reverse = words.reverse().join(" ");
